719fe5ebb727d3913d6d103a9018ecf6f9e78dc4
Junior-Jurado <juniorjurado2004@gmail.com>
2023-11-11 16:19:12 -0500

    Se implementa el drawer pero inhabilita el click de botones

--- app/src/main/java/com/example/meeteducation/DahboardFragment.kt
+++ app/src/main/java/com/example/meeteducation/DahboardFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.MenuProvider
+import androidx.core.view.isVisible
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.lifecycleScope
 import com.example.meeteducation.database.AppDatabase
 import com.example.meeteducation.database.user.UserDao
@@ -15,6 +17,7 @@ import kotlinx.coroutines.launch
 class DahboardFragment : Fragment() {
 
     private var _binding: FragmentDahboardBinding? = null
+    private lateinit var drawer: DrawerLayout
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -42,6 +45,9 @@ class DahboardFragment : Fragment() {
                 println("${user.id}. ${user.email}, ${user.rol}, ${user.password}, ${user.user}")
             }
         }
+        /*drawer = requireActivity().findViewById(R.id.drawer_navigation)
+        drawer.isVisible = true*/
+
         return binding.root
     }
 
--- app/src/main/java/com/example/meeteducation/MainActivity.kt
+++ app/src/main/java/com/example/meeteducation/MainActivity.kt
@@ -1,10 +1,27 @@
 package com.example.meeteducation
 
+import android.content.Context
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.meeteducation.databinding.ActivityMainBinding
+import android.os.PersistableBundle
+import android.view.MenuItem
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.fragment.NavHostFragment
 
-class MainActivity : AppCompatActivity() {
+import com.example.meeteducation.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var drawer: DrawerLayout
+    private lateinit var toggle: ActionBarDrawerToggle
 
     private lateinit var binding: ActivityMainBinding
 
@@ -12,5 +29,65 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        /*val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        drawer = findViewById(R.id.drawer_navigation) // Agregado para inicializar drawer
+
+        toggle = ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer.addDrawerListener(toggle)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+
+
+
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+
+        supportActionBar?.hide()
+
+        val sharedPreferences = getSharedPreferences("MyAppPreferences", Context.MODE_PRIVATE)
+        val username = sharedPreferences.getString("user", "Nombre Predeterminado")
+
+        // Configura el header del NavigationView con el nombre de usuario
+        val headerView = navigationView.getHeaderView(0)
+        val usernameTextView: TextView = headerView.findViewById(R.id.textViewUsername)
+        usernameTextView.text = username
+
+
+         */
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.nav_btn_saved -> Toast.makeText(this,"Item saved", Toast.LENGTH_SHORT).show()
+            R.id.nav_btn_config -> Toast.makeText(this,"Item config", Toast.LENGTH_SHORT).show()
+            R.id.nav_btn_help -> Toast.makeText(this,"Item help", Toast.LENGTH_SHORT).show()
+            R.id.nav_btn_logout -> Toast.makeText(this,"Item logout", Toast.LENGTH_SHORT).show()
+        }
+        drawer.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onPostCreate(savedInstanceState, persistentState)
+        toggle.syncState()
+    }
+
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        toggle.onConfigurationChanged(newConfig)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
