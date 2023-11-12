b361f80b0583c243510d8b26114ead8de0c0c43d
Junior-Jurado <juniorjurado2004@gmail.com>
2023-11-11 22:49:53 -0500

    implementacion de los likes con bases de datos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("androidx.navigation.safeargs.kotlin")
     // Add KSP for Kotlin
     id("com.google.devtools.ksp")
+
 }
 
 android {
@@ -46,6 +47,7 @@ android {
     }
 }
 
+
 dependencies {
 
 
@@ -67,16 +69,17 @@ dependencies {
     implementation("androidx.room:room-ktx:$roomVersion")
 
     // Navigation Component
-    val navVersion = "2.7.4"
+    val navVersion = "2.7.5"
     implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
     implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
 
     // Core Default
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
